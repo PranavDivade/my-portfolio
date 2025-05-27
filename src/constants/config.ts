981727d7d486c8f712081b621cf3a0f520182c16
Pranav Divade <divadepranav@gmail.com>
2025-05-27 15:29:07 +0530

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -6,7 +6,7 @@ const LocalConfig = {
     NEXT_PUBLIC_GTAG_ID: process.env.NEXT_PUBLIC_GTAG_ID,
     NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
     NEXT_PUBLIC_VERCEL_TOKEN: process.env.NEXT_PUBLIC_VERCEL_TOKEN,
-    NEXT_PUBLIC_RESUME_LINK: process.env.NEXT_PUBLIC_RESUME_LINK || "https://my-portfolio-omega-fawn-89.vercel.app/pranav resume.pdf",
+    NEXT_PUBLIC_RESUME_LINK: "https://my-portfolio-omega-fawn-89.vercel.app/pranav resume.pdf",
   },
 };
 
